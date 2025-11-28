f541f773e172517f3e8aa6031140cf0073bbac54
Legatt01 <jostynjaye14@gmail.com>
2025-11-28 16:27:30 +0100

    Esta es la App.

---
+++ App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Vuelo vuelo1 = new Vuelo(null, null, null, 0);
+        Vuelo vuelo2 = new Vuelo(null, null, null, 0);
+    }
+}
